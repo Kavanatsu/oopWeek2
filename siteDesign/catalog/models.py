fe6e6b32a30c8639b3e05fb8ae13c08c16f545f2
Kavanatsu <20062000@inbox.ru>
2022-10-21 09:22:13 +0700

    1.2

--- siteDesign/catalog/models.py
+++ siteDesign/catalog/models.py
@@ -1,5 +1,11 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import FileExtensionValidator
 from django.db import models
+from django.utils.crypto import get_random_string
+
+
+def get_name_file(instance, filename):
+    return '/'.join([get_random_string(length=5) + '_' + filename])
 
 
 # Create your models here.
@@ -7,4 +13,21 @@ class User(AbstractUser):
     name = models.CharField(max_length=254, verbose_name='Имя', blank=False)
     surname = models.CharField(max_length=254, verbose_name='Фамилия', blank=False)
     patronymic = models.CharField(max_length=254, verbose_name='Отчество', blank=True)
+    username = models.CharField(max_length=254, verbose_name='Логин', unique=True, blank=False)
+    email = models.CharField(max_length=254, verbose_name='Почта', unique=True, blank=False)
+    password = models.CharField(max_length=254, verbose_name='Пароль', blank=False)
+    role = models.CharField(max_length=254, verbose_name='Пароль',
+                            choices=(('admin','Администратор'),('user','Пользователь')), default='user')
 
+    USERNAME_FIELD = 'username'
+
+    def __str__(self):
+        return str(self.name) + " " + str(self.surname)
+
+class Order(models.Model):
+    name = models.CharField(max_length=254, verbose_name='Имя', blank=False)
+    description = models.TextField(max_length=254,)
+    date = models.DateTimeField(verbose_name='Дата добавления', auto_now_add=True)
+    photo_file = models.ImageField(max_length=254, upload_to=get_name_file,
+                                   blank=True, null=True,
+                                   validators=[FileExtensionValidator(allowed_extensions=['png', 'jpg', 'jpeg', 'bmp'])])
